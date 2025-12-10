76475f08ac5421f00648b66fe2fc5e8b7caf7b2c
Kaila Bryant <k.bryant45610@gmail.com>
2025-12-10 16:28:40 -0500

    getting setup for deployment to github pages

--- src/components/Visual/Visual.js
+++ src/components/Visual/Visual.js
@@ -1,5 +1,9 @@
 import photo from "../../Assets/WIP.png"
 import MobileInterface from "./MobileInterface";
+import WebInterface from "./WebInterface";
+import ezmng from "./Assets/EzMng Prototype.gif"
+import handi from "./Assets/HandiBot.png"
+import starbit from "./Assets/starbit cafe.png"
 function Visual() {
     return(
         <main className="visual">
@@ -7,25 +11,24 @@ function Visual() {
                 <h1>My Designs</h1>
                 <p>My mobile, web, and other designs will be displayed on this page!</p>
             </section>
+            <h2 className="title" style={{backgroundColor:"transparent", border:"none", marginBottom: "2px"}}>Web Interfaces</h2>
             <div className="designs-container">
-                <MobileInterface
+                <WebInterface
                     title="Title"
-                    interface={photo}
+                    interface={ezmng}
                     description="description"
                     tools="tools"
                     />
-                    <MobileInterface
+                    <WebInterface
                     title="Title"
-                    interface={photo}
+                    interface={handi}
                     description="description"
                     tools="tools"
                     />
-                    <MobileInterface
-                    title="Title"
-                    interface={photo}
-                    description="description"
-                    tools="tools"
-                    />
+            </div>
+            <h2 className="title" style={{backgroundColor:"transparent", border:"none", marginBottom: "2px"}}>More On the Way!</h2>
+            <div style={{paddingBottom:"15vh"}}>
+                <img src={photo} style={{display:"grid",justifySelf:"center"}}/>
             </div>
         </main>
     )
--- src/components/Visual/WebInterface.js
+++ src/components/Visual/WebInterface.js
@@ -2,8 +2,10 @@ function WebInterface(props){
     return(
         <div className="web-interface">
             <h3>{props.title}</h3>
-            <img src={props.interface} alt="web interface"/>
             <p>{props.description}</p>
+            <section className="imgBox">
+            <img src={props.interface} alt="web interface"/>
+            </section>
             <p className="design-tools">{props.tools}</p>
         </div>
     )
